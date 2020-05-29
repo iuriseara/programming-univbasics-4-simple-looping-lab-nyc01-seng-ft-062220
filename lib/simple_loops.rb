b9e8288586df36ff4dce01bead12a0308ba02045
fearless-command-2358 <iuriseara@gmail.com>
2020-05-29 16:49:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,10 +17,20 @@ def loop_message_n_times(message, limit)
   end
 end
 
+
 def output_array(array)
   count = 0
   while count < array.length do
     puts array
+    count += 1
+  end
+end
+
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count].to_s
     count += 1
   end
 end
